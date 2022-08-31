80fa8bc1cd6a0d3d9efbbc01891df56d8c4b6691
rickstaryo <qnrud16@gmail.com>
2022-08-31 21:40:12 +0900

    Sending a message
    1.Backend to Frontend
    2.FrontEnd to Backend
    3. Connecting The Server and Disconnecting the Server
    4. Using Console to Check the Websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,20 @@
 const frontSocket = new WebSocket(`ws://localhost:3000`);
+
+// 1.Conecting 'Server' Browser to FrontEnd
+frontSocket.addEventListener("open", ()=>{
+    console.log("Connected to sever. ✔️")
+})
+
+// 2. Showing message on Console
+frontSocket.addEventListener("message", (message)=>{
+    console.log("New Message", message.data);
+})
+
+// 3. Closing the 'Server' Backend to FrontEnd 
+frontSocket.addEventListener("close" ,()=>{
+    console.log("DisConnected to sever. ")
+})
+
+setTimeout(()=>{
+    frontSocket.send("hello from the browser");
+},10000)
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import { WebSocketServer } from 'ws'
 import express from "express";
+import { Socket } from "dgram";
 
 
 const app = express();
@@ -18,10 +19,20 @@ const server = http.createServer(app);
 // web socket Server 
 const wss = new WebSocketServer({ server });
 
-function handleConnection(backsocket){
-    console.log(backsocket);
-}
+wss.on("connection",(backSocket)=>{
+    //1. Registering a listener When Browser Connect to server
+    console.log("We got connected to Browser");
 
-wss.on("connection",handleConnection);
+    //2.  Registering a listener When Browser Disconncet to Server
+    backSocket.on("close", ()=>{
+        console.log("Disconnected from Browser")
+    });
+    // 3. Resgistering if fronend send a message to backEnd 
+    backSocket.on("message",(message)=>{
+        console.log(message.toString('utf8'));
+    });
+    // 4. Sending a message to frontend 
+    backSocket.send("hello!!");
+});
 
 server.listen(3000, handleListen);
