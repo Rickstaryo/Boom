9526bf3115d3c5e8d3b0d6dd6255627d576a3674
rickstaryo <qnrud16@gmail.com>
2022-08-31 18:39:59 +0900

    SettingWebSocketServer
    SettingWebSocketServer on the top of the Http Server

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
+import http from "http";
+import { WebSocketServer } from 'ws'
 import express from "express";
 
 
 const app = express();
 
-console.log("hello zoom")
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname+"/public"));
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(3000);
+const handeListen = () => console.log("Listening on http://localhost:3000");
+
+// this is http server
+const server = http.createServer(app);
+// web socket Server 
+const wss = new WebSocketServer({ server });
+
+server.listen(3000,handeListen);
