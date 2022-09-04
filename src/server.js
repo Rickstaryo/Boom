d3350453413fbd425cfa42937aea7952ae3839cb
rickstaryo <qnrud16@gmail.com>
2022-09-04 22:56:41 +0900

    ws to change Libarary Socket.IO
    Connecting by SocketIO

--- src/server.js
+++ src/server.js
@@ -1,54 +1,64 @@
 import http from "http";
-import { WebSocketServer } from 'ws'
+import SocketIO from "socket.io";
 import express from "express";
 import { Socket } from "dgram";
 
 
 const app = express();
-
+np
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname+"/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log("Listening on http://localhost:3000");
-
 // this is http server
-const server = http.createServer(app);
-// web socket Server 
-const wss = new WebSocketServer({ server });
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-function onSocketClose(){
-    console.log("DisConnected From the Browser");
-}
-//0. Message save Container
-const messageSockets=[];
+wsServer.on("connection", (socket) => {
+    console.log(socket);
+});
 
-
-wss.on("connection", (backSocket) => {
-    messageSockets.push(backSocket);
-    backSocket["nickname"] = "Anon";
-    console.log("Connected to Browser ✅");
-    backSocket.on("close", onSocketClose);
-    backSocket.on("message", (msg) => {
-        const message = JSON.parse(msg);
-        switch (message.type) {
-            case "new_message":
-            messageSockets.forEach((aSocket) =>
-                aSocket.send(`${backSocket.nickname}: ${message.payload}`)
-            );
-            break;
-            case "nickname":
-            backSocket["nickname"] = message.payload;
-            break;
-        }
-        });
-});   
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
 
 
 
-server.listen(3000, handleListen);
+
+//Comparing the Socket.IO
+// const handleListen = () => console.log("Listening on http://localhost:3000");
+// // web socket Server 
+// const wss = new WebSocketServer({ server });
+
+// function onSocketClose(){
+    //     console.log("DisConnected From the Browser");
+// }
+// //0. Message save Container
+// const messageSockets=[];
+
+
+// wss.on("connection", (backSocket) => {
+//     messageSockets.push(backSocket);
+//     backSocket["nickname"] = "Anon";
+//     console.log("Connected to Browser ✅");
+//     backSocket.on("close", onSocketClose);
+//     backSocket.on("message", (msg) => {
+//         const message = JSON.parse(msg);
+//         switch (message.type) {
+//             case "new_message":
+//             messageSockets.forEach((aSocket) =>
+//                 aSocket.send(`${backSocket.nickname}: ${message.payload}`)
+//             );
+//             break;
+//             case "nickname":
+//             backSocket["nickname"] = message.payload;
+//             break;
+//         }
+//         });
+// });   
+
+
 
 
 
