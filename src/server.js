2d012785ccbd91b5bba040c26f5f78809533956f
rickstaryo <qnrud16@gmail.com>
2022-09-05 08:16:53 +0900

    Room build &Feature Testing
    1. Can using Custom Events
    2. Can send an object
    3. Can use callback from BE

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import { Socket } from "dgram";
 
 
 const app = express();
-np
+
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname+"/public"));
@@ -17,8 +17,14 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-    console.log(socket);
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        setTimeout(() => {
+        done();
+        }, 10000);
+    });
 });
+
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
