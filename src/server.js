3e00a3b1b68586a5738177863866fddeb18ec9a0
rickstaryo <qnrud16@gmail.com>
2022-09-05 22:58:23 +0900

    Build a Room & Name a Room
    1. Using the join method to build a room by SocketIO library
    2. Using the hidden and show method to show up the message and then putting input

--- src/server.js
+++ src/server.js
@@ -5,23 +5,35 @@ import { Socket } from "dgram";
 
 
 const app = express();
-np
+
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname+"/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
+
+
 // this is http server
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
+
 wsServer.on("connection", (socket) => {
-    console.log(socket);
+    socket.onAny((event)=>{
+        console.log(`Socket events:${event}`)
+    })
+    socket.on("enter_room", (roomName, done) => {
+            socket.join(roomName);
+            done();
+
+    });
 });
+
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 httpServer.listen(3000, handleListen);
+
 
 
 
