482a96c9b224558868d237b1e3aae00792f27476
rickstaryo <qnrud16@gmail.com>
2022-09-07 20:22:41 +0900

    Pull out NickName to Room Section

--- src/server.js
+++ src/server.js
@@ -20,29 +20,25 @@ const wsServer = SocketIO(httpServer);
 
 
 wsServer.on("connection", (socket) => {
-    // If people didn't put nickname entering room 
-    socket["nickname"]= "Anon"
-    socket.onAny((event)=>{
-        console.log(`Socket events:${event}`)
+    socket["nickname"] = "Anonymous";
+    socket.onAny((event) => console.log(`event: ${event}`));
+    socket.on("enter_room", (roomName, userName, done) => {  
+      socket.join(roomName);
+      socket["nickname"] = userName;
+      done();
+      socket.to(roomName).emit("welcome", socket.nickname);
+    });
+    socket.on("disconnecting", ()=>{
+      socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname));
+    });
+    socket.on("new_msg", (msg, room, done) => {
+      socket.to(room).emit("new_msg", `${socket.nickname}: ${msg}`);
+      done();
+    });
+    socket.on("nickname", nickname => {
+      socket["nickname"] = nickname;
     })
-    socket.on("enter_room", (roomName, done) => {
-            socket.join(roomName);
-            done();
-            socket.to(roomName).emit("welcome", socket.nickname);
-        })
-    // Disconnecting 
-    socket.on("disconnecting", ()=> {
-        socket.rooms.forEach(room =>{ socket.to(room).emit("bye", socket.nickname);
-            
-        });
-    })
-    // Sending a msg to Backend
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
-        done();
-    })
-    socket.on("nickname", (nickname)=> (socket["nickname"]=nickname))
-});
+  });
 
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
